e071dd44e58dddff0ae9164b2524d5542788fbc8
johan <johan.schantz@telia.com>
2017-04-19 13:15:49 +0200

    changed method name

--- app/controllers/api/v1/cfhs_controller.rb
+++ app/controllers/api/v1/cfhs_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::CfhsController < ApplicationController
   def create
-    @cfh = Cfh.new cfh_params
+    @cfh = Cfh.new help_request
     if @cfh.save
       render json: { message: "successful"}, status: 204
     end
   end
 
 private
-  def cfh_params
+  def help_request
     params.permit(:title, :message, :status)
   end
 end
