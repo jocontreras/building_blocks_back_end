eda9d520fa7486eda77cd990774b13fbf860f266
fottander <felix.ottander@yahoo.com>
2017-04-20 11:16:59 +0200

    Seed facilities (#12)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,10 @@ News.create(title: "Casper", description: "Has no pants because of pants fire")
 News.create(title: "Playground", description: "Playground is being repaired")
 News.create(title: "Cleaning", description: "Time for spring cleaning!")
 
+Facility.create(name: "Sauna", description: "Awesome sauna", rules: "No underwear")
+Facility.create(name: "Laundry", description: "Awesome laundry room", rules: "Leave it as you found it")
+Facility.create(name: "Roof terrace", description: "Sunny terrace", rules: "No smoking")
+Facility.create(name: "Backyard", description: "Backyard with barbecue", rules: "No dogs allowed")
+
 puts "Created #{News.count} news"
+puts "Created #{Facility.count} facilities"
