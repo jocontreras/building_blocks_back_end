f5aa6e4af70c07d5df2e807f386fcb43d40c936f
casperjureskog <lcasper@mac.com>
2017-04-24 16:07:22 +0200

    add name fix (#21)

--- db/migrate/20170421130708_devise_token_auth_change_users.rb
+++ db/migrate/20170421130708_devise_token_auth_change_users.rb
@@ -1,4 +1,4 @@
-class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.0]
+class DeviseTokenAuthChangeUsers < ActiveRecord::Migration[5.0]
   def change
     change_table(:users) do |t|
       ## Required
